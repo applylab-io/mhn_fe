31940d54ca867c1a93b1f1a471f59bec10a5e41a
NKS <niranjan.sinha@applylab.io>
2018-11-09 07:42:08 +0530

    Contact added with reCaptcha

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {Component} from "react";
 import "../../css/App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import * as emailjs from "emailjs-com";
 import HomePageView from "./HomePageView";
 import PartnersPageView from "./PartnersPageView";
 import MusicPageView from "./MusicPageView";
@@ -12,26 +13,34 @@ import VideosPageView from "./VideoPageView";
 import ContactPageView from "./ContactPageView";
 import NoMatch from "./NoMatch";
 
-const App = () => (
-    <Router>
-        <div className='app'>
-            <Header />
-            <div className="content">
-                <Switch>
-                    <Route exact path="/" component={HomePageView} />
-                    <Route path="/partners" component={PartnersPageView} />
-                    <Route path="/music" component={MusicPageView} />
-                    <Route path="/videos" component={VideosPageView} />
-                    <Route path="/news" component={NewsPageView} />
-                    <Route path="/about" component={AboutPageView} />
-                    <Route path="/contact" component={ContactPageView} />
-                    {/* when none of the above match, <NoMatch> will be rendered */}
-                    <Route component={NoMatch} />
-                </Switch>
+class App extends Component {
+    constructor() {
+        super();
+        emailjs.init("user_5JiSj8EUURp7dGwXrfeoW");
+    }
+    render() {
+        return(
+            <Router>
+            <div className='app'>
+                <Header />
+                <div className="content">
+                    <Switch>
+                        <Route exact path="/" component={HomePageView} />
+                        <Route path="/partners" component={PartnersPageView} />
+                        <Route path="/music" component={MusicPageView} />
+                        <Route path="/videos" component={VideosPageView} />
+                        <Route path="/news" component={NewsPageView} />
+                        <Route path="/about" component={AboutPageView} />
+                        <Route path="/contact" component={ContactPageView} />
+                        {/* when none of the above match, <NoMatch> will be rendered */}
+                        <Route component={NoMatch} />
+                    </Switch>
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
-    </Router>
-);
+        </Router>            
+        );
+    }
+}
 
 export default App;
--- src/js/components/ContactPageView.js
+++ src/js/components/ContactPageView.js
@@ -1,22 +1,41 @@
 import React, { Component } from "react";
+import * as emailjs from "emailjs-com";
+import "../../css/ContactPageView.css";
 
 class ContactPageView extends Component {
+    constructor(props) {
+        super(props);
+        this.contact_number = null;
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+    handleFormSubmit(e) {
+        e.preventDefault();
+        this.contact_number.value = Math.random() * 100000 | 0;
+        emailjs.sendForm("gmail_applylab", "contact_form", "#contact-form")
+            .then(function (response) {
+                console.log("SUCCESS!", response.status, response.text);
+            }, function (err) {
+                console.log("FAILED...", err);
+            });
+    }
     render() {
         return (
-            <div style={baseStyle}>
-                <h2 style={{textAlign: "center"}}>This is contact page</h2>
+            <div className="contact-page-view">
+                <h1 style={{ textAlign: "center" }}>Contact Us</h1>
+                <form id="contact-form" onSubmit={this.handleFormSubmit}>
+                    <input type="hidden" name="contact_number" ref={c => (this.contact_number = c)} />
+                    <label htmlFor="user_name">Name</label>
+                    <input id="user_name" type="text" name="user_name" />
+                    <label htmlFor="user_email">Email</label>
+                    <input id="user_email" type="email" name="user_email" />
+                    <label htmlFor="contact">Message</label>
+                    <textarea id="text" name="text"></textarea>
+                    <div className="g-recaptcha" data-sitekey="6LeHoXkUAAAAAARqXmFJZR_9SfZRTH8nDaetTLXR"></div>
+                    <input type="submit" value="Send" />
+                </form>
             </div>
         );
     }
 }
-
-const baseStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    flexWrap: 'wrap',
-    width: '100%',
-    maxWidth: '100%'
-};
 
 export default ContactPageView;
